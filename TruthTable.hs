7ef330e2be1381d0ae586a425004b184307a3d65
Raziel <lordsoulreaver@googlemail.com>
2016-09-21 14:45:15 +0200

    TruthTable added

---
+++ TruthTable.hs
@@ -0,0 +1,27 @@
+module TruthTable
+where
+
+import           Data.List (intercalate)
+
+
+type TruthTable = [[Bool]]
+
+-- | generate a truth table for n variables
+
+truthTable :: Int -> TruthTable
+truthTable n
+  | n == 0 = [[]]
+  | otherwise = map (True :) t' ++ map (False :) t'
+  where
+        t' = truthTable (n-1)
+
+ppTruthTable :: TruthTable -> String
+ppTruthTable tt
+  = unlines (map ppRow tt)
+  where
+    ppRow r = intercalate " " (map (take 1 . show) r)
+
+printTT :: TruthTable -> IO ()
+printTT = putStrLn . ppTruthTable
+
+-- ----------------------------------------
